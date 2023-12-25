ec01bb912b73e25c4b0837dd50c70f806b32069e
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-25 06:08:49 +0200

    implemented user model

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,16 @@ const app = express();
 const { connectToMongoDb } = require("./configs/mongoDb.configs");
 app.use(express.json());
 require("dotenv").config();
+const fileUpload = require("express-fileupload");
+app.use(fileUpload());
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.listen(8000, () => {
   console.log("listening");
   connectToMongoDb();
