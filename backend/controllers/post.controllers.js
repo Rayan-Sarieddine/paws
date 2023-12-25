96c017713584c2d3a56932bbeab2a7bfcef6d37c
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-25 17:41:14 +0200

    implemented post edit functionality

--- backend/controllers/post.controllers.js
+++ backend/controllers/post.controllers.js
@@ -61,7 +61,76 @@ const addPost = async (req, res) => {
   }
 };
 
-const editPost = async (req, res) => {};
+const editPost = async (req, res) => {
+  let { description, location, type, image, id } = req.body;
+  let updatedValues = {};
+  try {
+    const post = await Post.findOne({ _id: id });
+    if (!post) {
+      return res.status(404).send({ message: "post not found" });
+    }
+
+    if (type) {
+      const validtypes = ["LOST", "FOUND"];
+      if (!validtypes.includes(type)) {
+        return res.status(400).send({ message: "type does not exist" });
+      }
+      updatedValues.type = type;
+    }
+    if (location) {
+      const validLocations = [
+        "BEIRUT",
+        "SOUTH",
+        "NORTH",
+        "BEKAA",
+        "MOUNT LEBANON",
+        "OTHER",
+      ];
+      if (!validLocations.includes(location)) {
+        return res
+          .status(400)
+          .send({ message: "location does not match available ones" });
+      }
+      updatedValues.location = location;
+    }
+    if (description) {
+      if (description.length < 5) {
+        return res
+          .status(400)
+          .send({ message: "not enough information given" });
+      }
+      updatedValues.description = description;
+    }
+
+    if (req.files && req.files.image) {
+      if (Array.isArray(req.files.image)) {
+        return res
+          .status(400)
+          .send({ message: "Only one image can be uploaded at a time" });
+      }
+      const imageFile = req.files.image;
+
+      const imageExtension = path.extname(imageFile.name);
+      const imageName = `${id}-${Date.now()}${imageExtension}`;
+
+      const imageDir = path.join(
+        __dirname,
+        "../public/images/posts",
+        imageName
+      );
+      await imageFile.mv(imageDir).catch((err) => {
+        console.error(err);
+        return res.status(500).send({ message: "Error uploading image" });
+      });
+      updatedValues.image = imageName;
+    }
+    await Post.findByIdAndUpdate(id, updatedValues);
+    return res.status(200).send({ message: "post updated" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ message: "server error" });
+  }
+};
 const getAllPosts = async (req, res) => {};
 const getPost = async (req, res) => {};
 const deletePost = async (req, res) => {};
