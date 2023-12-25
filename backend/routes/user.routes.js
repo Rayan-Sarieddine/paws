e45b985b58cfef8b999834bf2257288d251d94b5
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-25 07:45:27 +0200

    updated user routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,18 @@
 const express = require("express");
-const { login, register } = require("../controllers/user.controllers");
+const {
+  login,
+  register,
+  updateUser,
+  getCart,
+  emptyCart,
+  addProductToCart,
+} = require("../controllers/user.controllers");
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/register", register);
+router.put("/:id", updateUser);
+router.get("/cart", getCart);
+router.post("/emptyCart", emptyCart);
+router.post("/addTocart", addProductToCart);
 module.exports = router;
