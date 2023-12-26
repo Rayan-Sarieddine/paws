89854302a572c3781b8aad726d4000e1adbe26ec
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-26 09:51:46 +0200

    implemented helpers

---
+++ frontend/src/core/helpers/request.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { local } from "./localstorage";
+
+axios.defaults.baseURL = "http://localhost:8000/api";
+
+export const sendRequest = async ({ route, method = "GET", body }) => {
+  const type = local("type");
+  const token = local("token");
+
+  const authorizationHeader = `${type} ${token}`;
+
+  console.log(authorizationHeader);
+
+  const response = await axios.request({
+    url: route,
+    method,
+    data: body,
+    headers: {
+      Authorization: authorizationHeader,
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+  return response.data;
+};
