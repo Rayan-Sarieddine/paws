6f30da753658f42f80725b3d91c5c3aeef8a2d42
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-26 15:00:01 +0200

    implemented nav routing

--- frontend/src/domain/components/common/Nav/index.jsx
+++ frontend/src/domain/components/common/Nav/index.jsx
@@ -1,21 +1,51 @@
 import React, { useState } from "react";
 import "./style.css";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Button from "../Button";
 function Nav() {
   //for the on click event of the nav elements
-  const [activeLink, setActiveLink] = useState("");
+  const [activeLink, setActiveLink] = useState("Home");
   const handleClick = (name) => {
     setActiveLink(name);
+    switch (name) {
+      case "Adopt":
+        navigate("/adopt");
+        break;
+      case "Home":
+        navigate("/");
+        break;
+      case "Shop":
+        navigate("/shop");
+        break;
+      case "Chat with Us":
+        navigate("/chat");
+        break;
+      case "Lost & Found":
+        navigate("/lost-found-main");
+        break;
+      default:
+    }
   };
   //log in navigation
-  const logIn = () => {};
+  let navigate = useNavigate();
+  const logIn = () => {
+    navigate("/login");
+  };
   //sign-up navigation
-  const signUp = () => {};
+  const signUp = () => {
+    navigate("/sign-up");
+  };
 
   return (
     <nav className="hero-nav">
       <div className="nav-logo">
-        <img src="./images/logo/logo-icon-transparent.png" alt="Home of Pets" />
+        <Link to="/">
+          <img
+            src="./images/logo/logo-icon-transparent.png"
+            alt="Home of Pets"
+          />
+        </Link>
       </div>
       <ul className="nav-links">
         {["Home", "Adopt", "Shop", "Lost & Found", "Chat with Us"].map(
