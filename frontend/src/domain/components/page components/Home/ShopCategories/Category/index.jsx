0018ec9f29c09d6d5003e1f49b60f683b6364d27
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-27 19:56:10 +0200

    implemented shop by category component

--- frontend/src/domain/components/page components/Home/ShopCategories/Category/index.jsx
+++ frontend/src/domain/components/page components/Home/ShopCategories/Category/index.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-
+import "./style.css";
 function Category({ name, image, icon }) {
   return (
-    <div class="category-card">
-      <div class="category-card-front">
-        <div class="category-icon">
-          <img src={`./images/categories/icons/${icon}`} alt="" />
+    <div className="container-card">
+      <div className="category-card">
+        <div className="category-card-front">
+          <div className="category-icon">
+            <img src={`./images/categories/icons/${icon}`} alt="" />
+          </div>
+          <div className="category-name">
+            <h4>{name}</h4>
+          </div>
         </div>
-        <div class="content">
-          <h4>Cat Supplies</h4>
+        <div className="category-card-back">
+          <img src={`./images/categories/images/h3-category-${image}`} alt="" />
         </div>
-      </div>
-      <div class="category-card-back">
-        <img src={`./images/categories/images/${image}`} alt="" />
       </div>
     </div>
   );
--- frontend/src/domain/components/page components/Home/ShopCategories/index.jsx
+++ frontend/src/domain/components/page components/Home/ShopCategories/index.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import "./style.css";
+import Category from "./Category";
 function ShopCategories() {
   return (
-    <div>
+    <div className="shop-by">
       <div className="grid-header">
         <img src="./favicon.png" alt="logo"></img>
         <h3>Shop By Category</h3>
       </div>
-      <div className="shop-catogories"></div>
+      <div className="shop-catogories">
+        <Category name="Dog Supplies" image="1.png" icon="dog.svg" />
+        <Category name="Cat Supplies" image="2.png" icon="cat.svg" />
+        <Category name="Bird Supplies" image="3.png" icon="bird.svg" />
+        <Category name="Fish Supplies" image="4.png" icon="fish.svg" />
+        <Category
+          name=" Small Animal Supplies"
+          image="5.png"
+          icon="Rabbit.svg"
+        />
+        <Category name="Accessories" image="6.png" icon="Acces.svg" />
+      </div>
     </div>
   );
 }
