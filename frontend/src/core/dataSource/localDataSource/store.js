8627b6f48271d7e34f96ca5fd5aafcc87f721d38
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-28 07:53:05 +0200

    implemented pet slice

--- frontend/src/core/dataSource/localDataSource/store.js
+++ frontend/src/core/dataSource/localDataSource/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer, { userSliceName } from "./user";
 import activeNavReducer, { activeNavSliceName } from "./active_nav";
+import petReducer, { petSliceName } from "./pet";
 //redux persistent
 
 export const store = configureStore({
   reducer: {
     [userSliceName]: userReducer,
     [activeNavSliceName]: activeNavReducer,
+    [petSliceName]: petReducer,
   },
   devTools: true,
 });
