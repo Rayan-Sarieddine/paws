dac44fc2f35cf7535b7fa3d3965775fd77b1bfa2
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-28 06:41:42 +0200

    implemented adopt page skeleton

---
+++ frontend/src/domain/components/page components/Adopt/Animals/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./style.css";
+function Animals() {
+  return <div>Animals</div>;
+}
+
+export default Animals;
--- frontend/src/domain/pages/Adopt/index.jsx
+++ frontend/src/domain/pages/Adopt/index.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import "./style.css";
 import HeaderImg from "../../components/common/HeaderImg";
 import Nav from "../../components/common/Nav";
+import AdoptPet from "../../components/page components/Adopt/AdoptPet";
+import Animals from "../../components/page components/Adopt/Animals";
+import Faq from "../../components/page components/Adopt/Faq";
+import Footer from "../../components/common/Footer";
 function Adopt() {
   return (
     <div>
       <Nav />
       <HeaderImg img_link="adopt-hero.png" />
+      <AdoptPet />
+      <Animals />
+      <Faq />
+      <Footer />
     </div>
   );
 }
