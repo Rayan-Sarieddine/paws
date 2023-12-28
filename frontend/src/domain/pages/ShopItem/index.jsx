94e9d9d16ea78b75c4321c5a072014916e6b3435
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-28 20:16:52 +0200

    implemented shop details component

--- frontend/src/domain/pages/ShopItem/index.jsx
+++ frontend/src/domain/pages/ShopItem/index.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Nav from "../../components/common/Nav";
+import HeaderImg from "../../components/common/HeaderImg";
+import Footer from "../../components/common/Footer";
+import Productinfo from "../../components/page components/ShopItem/ProductInfo";
 
 function ShopItem() {
-  return <div>ShopItem</div>;
+  return (
+    <div>
+      <Nav />
+      <HeaderImg img_link="shop-hero.png" />
+      <Productinfo />
+      <Footer />
+    </div>
+  );
 }
 
 export default ShopItem;
