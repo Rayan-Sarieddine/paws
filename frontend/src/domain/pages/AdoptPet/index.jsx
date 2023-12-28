7624be47a047066b9fac37ea3e46e4106e39fe43
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-28 17:52:10 +0200

    updated adopt pages

--- frontend/src/domain/pages/AdoptPet/index.jsx
+++ frontend/src/domain/pages/AdoptPet/index.jsx
@@ -4,10 +4,12 @@ import Nav from "../../components/common/Nav";
 import PetInfo from "../../components/page components/AdoptPet/PetInfo";
 import Footer from "../../components/common/Footer";
 import { useSelector } from "react-redux";
+import HeaderImg from "../../components/common/HeaderImg";
 function AdoptPet() {
   return (
     <div>
       <Nav />
+      <HeaderImg img_link="adopt-hero.png" />
       <PetInfo />
       <Footer />
     </div>
