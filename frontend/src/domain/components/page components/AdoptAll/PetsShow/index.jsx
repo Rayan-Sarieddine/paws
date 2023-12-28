d8d416254fa233aa35458595e71996519252da62
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-28 15:46:06 +0200

    implemented pagination v2

--- frontend/src/domain/components/page components/AdoptAll/PetsShow/index.jsx
+++ frontend/src/domain/components/page components/AdoptAll/PetsShow/index.jsx
@@ -1,28 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 import { useSelector } from "react-redux";
 
 function PetsShow() {
   const petData = useSelector((state) => state.Pet);
+  const [age, setAge] = useState(5);
+
   const [currentPage, setCurrentPage] = useState(1);
   const petsPerPage = 9;
+  const [filter, setFilter] = useState({ type: "all", age: "", status: "" });
+  const [filteredPets, setFilteredPets] = useState(petData.pets);
+
+  useEffect(() => {
+    let newFilteredPets = petData.pets;
+
+    if (filter.type !== "all") {
+      newFilteredPets = newFilteredPets?.filter(
+        (pet) => pet.type === filter.type
+      );
+    }
+
+    if (filter.age) {
+      newFilteredPets = newFilteredPets?.filter((pet) => pet.age <= filter.age);
+    }
+
+    setFilteredPets(newFilteredPets);
+  }, [filter, petData.pets]);
+
   const indexOfLastPet = currentPage * petsPerPage;
   const indexOfFirstPet = indexOfLastPet - petsPerPage;
-  const currentPets = petData?.pets.slice(indexOfFirstPet, indexOfLastPet);
-  const [filter, setFilter] = useState({ type: "", age: "", status: "" });
+  const currentPets = filteredPets?.slice(indexOfFirstPet, indexOfLastPet);
+
   const handleTypeChange = (e) => {
     setFilter({ ...filter, type: e.target.value });
   };
 
   const handleAgeChange = (e) => {
-    setFilter({ ...filter, age: e.target.value });
+    const newAge = parseInt(e.target.value);
+    setAge(newAge);
+    setFilter({ ...filter, age: newAge });
   };
-  const handleStatusChange = (e) => {
-    setFilter({ ...filter, status: e.target.value });
-  };
+
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
   const filterData = () => {};
-
+  useEffect(() => {}, [currentPets]);
   return (
     <div className="pets-show-container">
       <div className="filters">
@@ -33,8 +54,18 @@ function PetsShow() {
             <input
               type="radio"
               name="type"
-              value="dog"
-              checked={filter.type === "dog"}
+              value="all"
+              checked={filter.type === "all"}
+              onChange={handleTypeChange}
+            />
+            All
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="type"
+              value="dogs"
+              checked={filter.type === "dogs"}
               onChange={handleTypeChange}
             />
             Dogs
@@ -43,8 +74,8 @@ function PetsShow() {
             <input
               type="radio"
               name="type"
-              value="cat"
-              checked={filter.type === "cat"}
+              value="cats"
+              checked={filter.type === "cats"}
               onChange={handleTypeChange}
             />
             Cats
@@ -53,8 +84,29 @@ function PetsShow() {
             <input
               type="radio"
               name="type"
-              value="other"
-              checked={filter.type === "other"}
+              value="fish"
+              checked={filter.type === "fish"}
+              onChange={handleTypeChange}
+            />
+            Fish
+          </label>
+
+          <label>
+            <input
+              type="radio"
+              name="type"
+              value="rabbits"
+              checked={filter.type === "rabbits"}
+              onChange={handleTypeChange}
+            />
+            Rabbits
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="type"
+              value="others"
+              checked={filter.type === "others"}
               onChange={handleTypeChange}
             />
             Others
@@ -63,46 +115,16 @@ function PetsShow() {
         <div className="filter-category second-filter">
           <h3>Age</h3>
           <hr></hr>
-          <label>
-            <input
-              type="radio"
-              name="age"
-              value="puppy_kitten"
-              checked={filter.age === "puppy_kitten"}
-              onChange={handleAgeChange}
-            />
-            Puppy/Kitten
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="age"
-              value="young"
-              checked={filter.age === "young"}
-              onChange={handleAgeChange}
-            />
-            Young
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="age"
-              value="adult"
-              checked={filter.age === "adult"}
-              onChange={handleAgeChange}
-            />
-            Adult
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="age"
-              value="senior"
-              checked={filter.age === "senior"}
-              onChange={handleAgeChange}
-            />
-            Senior
-          </label>
+          <input
+            type="range"
+            id="ageSlider"
+            name="ageSlider"
+            min={1}
+            max={14}
+            value={age}
+            onChange={handleAgeChange}
+          />
+          <p>Age to be less than {age}</p>
         </div>
         <button className="btn btn-filter" onClick={filterData()}>
           Filter
@@ -111,9 +133,6 @@ function PetsShow() {
       <div className="pet-pagination-main">
         <div className="pet-pagination-header">
           <p>ADOPT</p>
-          <div className="pet-pagination-header_filter">
-            <p>Show</p>
-          </div>
         </div>
         <div className="pet-pagination-controls">
           <button
@@ -123,34 +142,41 @@ function PetsShow() {
           >
             &lt;
           </button>
-
+          <span>{currentPage}</span>
           <button
             className="btn btn-pagination-right"
             onClick={() => paginate(currentPage + 1)}
             disabled={
-              currentPage === Math.ceil(petData?.pets.length / petsPerPage)
+              currentPage === Math.ceil(petData?.pets?.length / petsPerPage)
             }
           >
             &gt;
           </button>
         </div>
-        <div className="pet-pagination-petCards">
-          {currentPets.map((pet, index) => (
-            <div key={index} className="pet-pagination-card">
-              <img
-                src={`http://localhost:8000/images/pets/${pet.image}`}
-                alt={pet.name}
-              />
-              <div className="pet-details">
-                <h3>{pet.name}</h3>
-                <p>
-                  {pet.description} | {pet.breed}
-                </p>
-                <button className="btn btn-adopt">VIEW</button>
+        {currentPets?.length === 0 ? (
+          <div className="no-pets-to-show">
+            <p>No Pets Found</p>
+          </div>
+        ) : (
+          <div className="pet-pagination-petCards">
+            {currentPets?.map((pet, index) => (
+              <div key={index} className="pet-pagination-card">
+                <img
+                  src={`http://localhost:8000/images/pets/${pet.image}`}
+                  alt={pet.name}
+                />
+                <div className="pet-details">
+                  <h3>{pet.name}</h3>
+                  <p>
+                    {pet.description} | {pet.breed}
+                  </p>
+                  <button className="btn btn-adopt">VIEW</button>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
+
         <div className="pet-pagination-controls">
           <button
             className="btn btn-pagination-left"
@@ -159,12 +185,12 @@ function PetsShow() {
           >
             &lt;
           </button>
-
+          <span>{currentPage}</span>
           <button
             className="btn btn-pagination-right"
             onClick={() => paginate(currentPage + 1)}
             disabled={
-              currentPage === Math.ceil(petData?.pets.length / petsPerPage)
+              currentPage === Math.ceil(petData?.pets?.length / petsPerPage)
             }
           >
             &gt;
