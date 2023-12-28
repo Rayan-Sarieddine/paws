4bcd552e62248247cc70afbc3b4b2cd1d766b812
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-28 17:24:58 +0200

    implemented confirm request page

--- frontend/src/domain/pages/AdoptRequest/index.jsx
+++ frontend/src/domain/pages/AdoptRequest/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Nav from "../../components/common/Nav";
+import RequestConfirmation from "../../components/page components/AdoptRequest/RequestConfirmation";
+import Footer from "../../components/common/Footer";
 
 function AdoptRequest() {
-  return <div>AdoptRequest</div>;
+  return (
+    <div>
+      <Nav />
+      <RequestConfirmation />
+      <Footer />
+    </div>
+  );
 }
 
 export default AdoptRequest;
