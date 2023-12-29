f543f55fcb810f2156d54176d24097035e8a9296
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-29 18:20:59 +0200

    implemented post slice

--- frontend/src/core/dataSource/localDataSource/store.js
+++ frontend/src/core/dataSource/localDataSource/store.js
@@ -3,6 +3,7 @@ import userReducer, { userSliceName } from "./user";
 import activeNavReducer, { activeNavSliceName } from "./active_nav";
 import petReducer, { petSliceName } from "./pet";
 import productReducer, { productSliceName } from "./product";
+import postReducer, { postSliceName } from "./post";
 //redux persistent
 
 export const store = configureStore({
@@ -11,6 +12,7 @@ export const store = configureStore({
     [activeNavSliceName]: activeNavReducer,
     [petSliceName]: petReducer,
     [productSliceName]: productReducer,
+    [postSliceName]: postReducer,
   },
   devTools: true,
 });
--- frontend/src/domain/pages/LostFoundManual/index.jsx
+++ frontend/src/domain/pages/LostFoundManual/index.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./style.css";
+import { useDispatch } from "react-redux";
+import { postsDataSource } from "../../../core/dataSource/remoteDataSource/posts";
+import Nav from "../../components/common/Nav";
+import HeaderImg from "../../components/common/HeaderImg";
+import Footer from "../../components/common/Footer";
 function LostFoundManual() {
-  return <div>LostFoundManual</div>;
+  const dispatch = useDispatch();
+
+  const loadpets = async () => {
+    try {
+      const response = await postsDataSource.getPosts();
+      console.log(response);
+      dispatch(loadPets(response.pets));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    loadpets();
+  }, []);
+  return (
+    <div>
+      <Nav />
+      <HeaderImg img_link="found-hero.png" />
+
+      <Footer />
+    </div>
+  );
 }
 
 export default LostFoundManual;
