b19b2fef7acb669347c7f5b60b4d1cab6114ea12
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-29 13:06:20 +0200

    implemented searching page

---
+++ frontend/src/domain/components/page components/LostFoundSearching/Searching/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+function Searching() {
+  return (
+    <div className="searching">
+      <div class="loader"></div>
+      <h4>Searching.....</h4>
+      <p>This might take a few minutes</p>
+    </div>
+  );
+}
+
+export default Searching;
--- frontend/src/domain/pages/LostFoundSearching/index.jsx
+++ frontend/src/domain/pages/LostFoundSearching/index.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import "./style.css";
+import Nav from "../../components/common/Nav";
+import HeaderImg from "../../components/common/HeaderImg";
+import Searching from "../../components/page components/LostFoundSearching/Searching";
+import Footer from "../../components/common/Footer";
 function LostFoundsearching() {
-  return <div>LostFoundsearching</div>;
+  return (
+    <div>
+      <Nav />
+      <HeaderImg img_link="found-hero.png" />
+      <Searching />
+      <Footer />
+    </div>
+  );
 }
 
 export default LostFoundsearching;
