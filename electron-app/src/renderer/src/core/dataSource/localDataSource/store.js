20afbb000bd9afe0a10ffda48faacc50ab291e36
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-03 23:31:37 +0200

    updates to pages folder

--- electron-app/src/renderer/src/core/dataSource/localDataSource/store.js
+++ electron-app/src/renderer/src/core/dataSource/localDataSource/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer, { userSliceName } from "./user";
-import activeNavReducer, { activeNavSliceName } from "./active_nav";
 import petReducer, { petSliceName } from "./pet";
 import productReducer, { productSliceName } from "./product";
 import postReducer, { postSliceName } from "./post";
@@ -10,7 +9,6 @@ import orderReducer, { orderSliceName } from "./order";
 export const store = configureStore({
   reducer: {
     [userSliceName]: userReducer,
-    [activeNavSliceName]: activeNavReducer,
     [petSliceName]: petReducer,
     [productSliceName]: productReducer,
     [postSliceName]: postReducer,
