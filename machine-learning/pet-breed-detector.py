288dec03a1545ebdce7a6e4abf346f51046b0032
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-04 15:51:03 +0200

    implemented reverse mapping to get prediction label

--- machine-learning/pet-breed-detector.py
+++ machine-learning/pet-breed-detector.py
@@ -140,3 +140,21 @@ image=img_to_array(image)
 image=image/255.0
 prediction_image=np.array(image)
 prediction_image= np.expand_dims(image, axis=0)
+
+#reverse mapping
+reverse_mapping={  0:'basset_hound', 1:'beagle', 2:'russian_blue', 3:'pomeranian', 4:'ragdoll',
+         5:'staffordshire_bull_terrier', 6:'keeshond', 7:'siamese', 8:'pug', 9:'shiba_inu',
+         10:'american_pit_bull_terrier', 11:'bengal', 12:'british_shorthair', 13:'newfoundland',
+         14:'havanese', 15:'japanese_chin', 16:'german_shorthaired', 17:'birman', 18:'maine_coon',
+         19:'english_cocker_spaniel', 20:'scottish_terrier', 21:'wheaten_terrier', 22:'chihuahua',
+         23:'american_bulldog', 24:'abyssinian', 25:'boxer', 26:'yorkshire_terrier', 27:'miniature_pinscher',
+         28:'sphynx', 29:'samoyed', 30:'leonberger', 31:'bombay', 32:'english_setter', 33:'persian',
+         34:'great_pyrenees', 35:'egyptian_mau', 36:'saint_bernard' }
+
+def mapper(value):
+    return reverse_mapping[value]
+
+prediction=model.predict(prediction_image)
+value=np.argmax(prediction)#np.argmax is used to find the index of the maximum value in the prediction array, which corresponds to the most likely class label.
+move_name=mapper(value)
+print("Prediction is {}.".format(move_name))
