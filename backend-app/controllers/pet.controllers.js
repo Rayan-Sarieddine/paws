1f87d433dbf032e61ec98247e316739d483ad4be
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-07 19:29:56 +0200

    backend-app <implemented pet controller structure>

---
+++ backend-app/controllers/pet.controllers.js
@@ -0,0 +1,13 @@
+const Pet = require("../models/pet.model");
+
+const addPet = async (req, res) => {};
+const editPet = async (req, res) => {};
+const deletePet = async (req, res) => {};
+const getPetofUser = async (req, res) => {};
+
+module.exports = {
+  addPet,
+  editPet,
+  deletePet,
+  getPetofUser,
+};
