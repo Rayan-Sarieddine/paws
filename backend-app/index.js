6f65a4702a181ea1efc71ebeec1058774c715d5b
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-07 18:22:51 +0200

    backend-app <implemented routes in index.js>

--- backend-app/index.js
+++ backend-app/index.js
@@ -23,6 +23,25 @@ app.use((req, res, next) => {
   next();
 });
 
+const authRoutes = require("./routes/auth.routes");
+app.use("/auth", authRoutes);
+
+//User Routes
+const userRoutes = require("./routes/user.routes");
+app.use("/user", userRoutes);
+
+//Tracker Routes
+const trackerRoutes = require("./routes/tracker.routes");
+app.use("/tracker", trackerRoutes);
+
+//Pet Routes
+const petRoutes = require("./routes/pet.routes");
+app.use("/pets", petRoutes);
+
+//Appointment Routes
+const appointmentRoutes = require("./routes/appointment.routes");
+app.use("/appointment", appointmentRoutes);
+
 const sequelize = require("./configs/db.configs");
 sequelize.sync().then(() => {
   app.listen(8000, () => {
--- backend-app/models/user.model.js
+++ backend-app/models/user.model.js
@@ -1,26 +1,44 @@
 const Sequelize = require("sequelize");
+const bcrypt = require("bcrypt");
 const sequelize = require("../configs/db.configs");
 
-const User = sequelize.define("user", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
+const User = sequelize.define(
+  "user",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
   },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  password: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true,
-  },
-});
+  {
+    hooks: {
+      beforeCreate: async (user) => {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      },
+      beforeUpdate: async (user) => {
+        if (user.changed("password")) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      },
+    },
+  }
+);
 module.exports = User;
