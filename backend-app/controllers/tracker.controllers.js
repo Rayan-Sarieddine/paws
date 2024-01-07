abb53b078e6d747b7cf22bb8498c2aeae1e5a68d
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-07 19:33:52 +0200

    backend-app <implemented tracker controller structure>

---
+++ backend-app/controllers/tracker.controllers.js
@@ -0,0 +1,13 @@
+const Tracker = require("../models/tracker.model");
+
+const addTracker = async (req, res) => {};
+const setLocation = async (req, res) => {};
+const deleteTracker = async (req, res) => {};
+const getLocation = async (req, res) => {};
+
+module.exports = {
+  addTracker,
+  setLocation,
+  deleteTracker,
+  getLocation,
+};
