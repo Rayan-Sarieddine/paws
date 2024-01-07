cfdb97a1999a9a9f4ed8fb89a372a581c080676a
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-07 16:42:11 +0200

    backend-app <implemented pet model>

---
+++ backend-app/models/pet.model.js
@@ -0,0 +1,29 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../configs/db.configs");
+
+const Pet = sequelize.define("pet", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  type: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  breed: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  image: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Pet;
