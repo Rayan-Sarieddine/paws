49637c501ff9ee419a77526d22c2422da7e937e2
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-07 12:29:06 +0200

    backend-app <implemented node dependencies>

--- backend-app/index.js
+++ backend-app/index.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const app = express();
 
+//To receive JSON
+app.use(express.json());
+
+//Dotenv dependencies
+require("dotenv").config();
+
+//File upload dependencies
+const fileUpload = require("express-fileupload");
+app.use(fileUpload());
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
+//CORS fix
+const cors = require("cors");
+app.use(cors({ origin: "*" }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.listen(8000, () => {
   console.log("LIVE");
 });
