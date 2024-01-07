f9707d6cc181bf23ca13e67518227f606f4b594f
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-07 19:28:31 +0200

    backend-app <implemented user controller structure>

---
+++ backend-app/controllers/auth.controllers.js
@@ -0,0 +1,12 @@
+const User = require("../models/user.model");
+
+//JWT dependencies
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const login = async (req, res) => {};
+const register = async (req, res) => {};
+
+module.exports = {
+  login,
+  register,
+};
