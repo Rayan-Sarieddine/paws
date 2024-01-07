38d6648c80285629414813ca232ecce6c748312e
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-07 14:07:46 +0200

    backend-app <implemented db connection>

---
+++ backend-app/configs/db.configs.js
@@ -0,0 +1,7 @@
+const { Sequalize } = require("sequelize");
+const sequalize = new Sequalize("db_paws_app", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+module.exports = sequalize;
