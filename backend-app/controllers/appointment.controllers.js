7c756a122e9cd9babdb25a6a084e2b0e189f8a66
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-07 19:49:27 +0200

    backend-app <implemented appointment controller structure>

---
+++ backend-app/controllers/appointment.controllers.js
@@ -0,0 +1,11 @@
+const Appointment = require("../models/appointment.model");
+
+const addAppointment = async (req, res) => {};
+const changeAppoitmentStatus = async (req, res) => {};
+const getAppointments = async (req, res) => {};
+
+module.exports = {
+  addAppointment,
+  changeAppoitmentStatus,
+  getAppointments,
+};
--- backend-app/routes/appointment.routes.js
+++ backend-app/routes/appointment.routes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const {
   addAppointment,
   changeAppoitmentStatus,
-
   getAppointments,
 } = require("../controllers/appointment.controllers");
 const router = express.Router();
