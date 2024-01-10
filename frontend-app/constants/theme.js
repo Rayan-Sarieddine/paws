2399095a3a75b7ebe9d91b17b85f54076df804a1
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-10 10:49:42 +0200

    frontend-app <implemented theme colors>

--- frontend-app/constants/theme.js
+++ frontend-app/constants/theme.js
@@ -1,4 +1,22 @@
 import { Dimensions } from "react-native";
 const { height, width } = Dimensions.get("window");
+export const COLORS = {
+  primary: "#018ae0",
+  primaryTint: "#1a96e3",
+  primaryShade: "#016eb3",
+  secondary: "#e12d5a",
+  secondaryTint: "#e4426b",
+  secondaryShade: "#cb2951",
+  black: "#212529",
+  grey: "#495057",
+  greyTint: " #868e96",
+  greyShade: "#343a40",
+  green: "#54cd38",
+  orange: "#ff9933",
+  purple: "#940c69",
+  yellow: "#fae053",
+  red: "#e32121",
+  white: "#f2f2f2",
+};
 
 export default appTheme;
