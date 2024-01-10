a4ea22b0bfdd5210c65281a79ddc0a62a11d80d7
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-10 14:44:20 +0200

    frontend-app <implemented welcome page header>

--- frontend-app/screens/Welcome.js
+++ frontend-app/screens/Welcome.js
@@ -1,17 +1,18 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, Dimensions } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import PageContainer from "../components/PageContainer";
 import { COLORS, images } from "../constants";
-
+const screenWidth = Dimensions.get("window").width;
 const Welcome = () => {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="light" />
       <PageContainer>
         <View style={styles.main}>
-          <Image source={images.logo} style={styles.imgMain} />
+          <Image source={images.screen2} style={styles.imgHeader} />
+          <Image source={images.logo} style={styles.imgLogo} />
         </View>
       </PageContainer>
     </SafeAreaView>
@@ -26,11 +27,16 @@ const styles = StyleSheet.create({
   main: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
   },
-  imgMain: {
+  imgLogo: {
     height: 120,
     width: 120,
+    marginBottom: 22,
+  },
+  imgHeader: {
+    height: 420,
+    width: screenWidth,
     marginBottom: 22,
   },
 });
