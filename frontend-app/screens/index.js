8b518339fadcbaa93bbdd0e26224acb4a7721d08
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-10 11:33:00 +0200

    frontend-app <implemented index.js ofscreens>

---
+++ frontend-app/screens/index.js
@@ -0,0 +1,8 @@
+import Home from "./Home";
+import Chat from "./Chat";
+import Login from "./Login";
+import Register from "./Register";
+import Profile from "./Profile";
+import Welcome from "./Welcome";
+
+export { Home, Chat, Login, Register, Profile, Welcome };
