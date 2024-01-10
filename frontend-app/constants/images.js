b90edbdc58b73093150a2a47c4b5062bf713cb49
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-10 10:43:43 +0200

    frontend-app <implemented images.js>

---
+++ frontend-app/constants/images.js
@@ -0,0 +1,15 @@
+const logo = require("../assets/images/favicon.png");
+const bathAppointment = require("../assets/images/bath.png");
+const vetAppointment = require("../assets/images/vet.png");
+const screen2 = require("../assets/images/screen2.png");
+const screen3 = require("../assets/images/screen3.png");
+const main = require("../assets/images/app-home.png");
+
+export default {
+  logo,
+  bathAppointment,
+  vetAppointment,
+  screen2,
+  screen3,
+  main,
+};
