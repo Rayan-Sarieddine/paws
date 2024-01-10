4e6b3ecd670859836362184c1dcc99a234b11bc7
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-10 14:30:57 +0200

    frontend-app <implemented welcome page structure>

--- frontend-app/screens/Welcome.js
+++ frontend-app/screens/Welcome.js
@@ -1,12 +1,37 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import PageContainer from "../components/PageContainer";
+import { COLORS, images } from "../constants";
 
 const Welcome = () => {
   return (
-    <View>
-      <Text>Welcome</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" />
+      <PageContainer>
+        <View style={styles.main}>
+          <Image source={images.logo} style={styles.imgMain} />
+        </View>
+      </PageContainer>
+    </SafeAreaView>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+  },
+  main: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imgMain: {
+    height: 120,
+    width: 120,
+    marginBottom: 22,
+  },
+});
 export default Welcome;
