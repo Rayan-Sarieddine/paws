54e13528da8f7137551d21af38d3262de50e55c0
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-13 21:08:02 +0200

    frontend-app <implemented nav for tracker>

--- frontend-app/screens/Tracker.js
+++ frontend-app/screens/Tracker.js
@@ -1,12 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import Nav from "../components/Nav";
 
 const Tracker = () => {
   return (
-    <View>
-      <Text>Tracker</Text>
+    <View style={styles.tracker}>
+      <Nav />
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+  tracker: {
+    flex: 1,
+  },
+});
 export default Tracker;
