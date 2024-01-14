2a50201b01883359dc26cf6baea565e16ec4c7fc
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-14 08:07:49 +0200

    frontend-app <implemented map showing>

--- frontend-app/screens/Tracker.js
+++ frontend-app/screens/Tracker.js
@@ -1,10 +1,21 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
 import React from "react";
 import Nav from "../components/Nav";
+import MapView from "react-native-maps";
 
 const Tracker = () => {
   return (
     <View style={styles.tracker}>
+      <MapView
+        style={styles.map}
+        region={{
+          latitude: 33.88863,
+          longitude: 35.49548,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        provider="google"
+      ></MapView>
       <Nav />
     </View>
   );
@@ -12,6 +23,10 @@ const Tracker = () => {
 const styles = StyleSheet.create({
   tracker: {
     flex: 1,
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
 export default Tracker;
