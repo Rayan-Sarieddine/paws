69fbd2577ab547c281693ce1f51a59e62e8f7d3f
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-14 08:27:59 +0200

    frontend-app <implemented getting user location>

--- frontend-app/screens/Tracker.js
+++ frontend-app/screens/Tracker.js
@@ -1,17 +1,32 @@
 import { View, Text, StyleSheet, Dimensions } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import Nav from "../components/Nav";
 import MapView from "react-native-maps";
 import * as Location from "expo-location";
+import { useNavigation } from "@react-navigation/native";
 
 const Tracker = () => {
+  const navigation = useNavigation();
   async function getLocationPermission() {
     let { status } = await Location.requestForegroundPermissionsAsync();
     if (status !== "granted") {
-      console.error("Permission to access location was denied");
+      navigation.navigate("Home");
       return;
     }
   }
+  async function getUserLocation() {
+    try {
+      await getLocationPermission();
+
+      let location = await Location.getCurrentPositionAsync({});
+      console.log(location);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  useEffect(() => {
+    getUserLocation();
+  }, []);
   return (
     <View style={styles.tracker}>
       <MapView
