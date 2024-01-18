d9ba4d13c14de245e31101f9048488a5e2b2b80c
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-18 16:58:27 +0200

    arduino <implemented gps stream in mock data sender>

--- aduino/mock_data_sender.ino
+++ aduino/mock_data_sender.ino
@@ -7,3 +7,7 @@
 const char* ssid     = "wifiName";       
 const char* password = "wifipassword";   
 
+const char *gpsStream =
+ "$GPRMC,045103.000,A,3535.5350,N,3340.2650,E,0.67,161.46,030913,,,A*56\r\n"
+  "$GPRMC,045103.000,A,3535.58084,N,3340.28808,E,0.67,161.46,030913,,,A*5F\r\n"
+  "$GPRMC,045103.000,A,3535.5194,N,3340.28136,E,0.67,161.46,030913,,,A*63\r\n";
