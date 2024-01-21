326fa6bb4616a4e8b4b1dca6da4877f3cd7ca418
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-21 11:48:19 +0200

    machine-learning <added readme file>

--- machine-learning/pet-breed-detector.py
+++ machine-learning/pet-breed-detector.py
@@ -1,12 +1,3 @@
-#Dataset:
-#Dataset consists of 37 different breeds of dogs and cats divided between training and testing, in the train, each folder is a breed(37 folders) and each breed folder contains 160 images of the specified breed (the label in the training is the folder name of the images).
-
-#In the testing it is a  total of random breeds divided to 1500 images with no labels given.
-
-#ML Paradign:
-#My model uses supervised learning, where you train a model on a labeled dataset. In this case, the dataset consists of images of various cat and dog breeds, each labeled with its corresponding breed. The model learns to associate images with these labels.
-
-#Purpose:The specific task here is classification, where the model is trained to categorize each image into one of several predefined classes (the different breeds)
 
 #imports needed
 import numpy as np
