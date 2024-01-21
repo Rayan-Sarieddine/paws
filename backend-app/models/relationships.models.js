a530b78c7dc1c4190dc4cb8152c817ae8ec78b86
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-01-21 13:34:35 +0200

    backend-app <updated realtionships>

--- backend-app/models/relationships.models.js
+++ backend-app/models/relationships.models.js
@@ -2,8 +2,10 @@ const User = require("./user.model");
 const Pet = require("./pet.model");
 const Tracker = require("./tracker.model");
 
+//User can have many pets, but a pet can belong to only one user
 User.hasMany(Pet, { foreignKey: "belongs_to" });
 Pet.belongsTo(User, { foreignKey: "belongs_to" });
 
+//Each pet can have 1 tracker, and a tracker can belong to only one pet
 Pet.hasOne(Tracker, { foreignKey: "pet_id" });
 Tracker.belongsTo(Pet, { foreignKey: "pet_id" });
